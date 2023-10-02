9ba753ef560aef8bfd3b8c46a0c2353a7a13bd80
Zack Steinkamp <zack@steinkamp.us>
2023-10-01 22:34:09 -0700

    fix alignment of filter box

--- frontend/src/components/ActivityGrid.tsx
+++ frontend/src/components/ActivityGrid.tsx
@@ -135,7 +135,7 @@ const ActivityGrid = ({ activities }: ActivityGridProps) => {
         className="datagrid pl-2 pr-2 pb-2 pt-2 w-full grid grid-cols-[2fr_4fr_1fr_1fr_1fr_1fr] md:grid-cols-[2fr_4fr_1fr_1fr_1fr_1fr_4fr]"
       >
         <div></div>
-        <div className="filter">
+        <div className="pl-2 pt-1 filter">
           <input
             onChange={handleFilterChange}
             type="text"
